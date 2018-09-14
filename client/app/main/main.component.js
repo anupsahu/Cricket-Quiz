41a8e2935dbe36b7f8c068353e89c840ec577ef2
Anup Sahu <anupsahu@cinqueon.com>
2018-09-14 12:48:23 +0530

    Validations

--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -9,27 +9,34 @@ export function MainController($scope) {
     this.questions = ques.ques;
 
     this.reset = function() {
+
+        //reset models
         this.answer = [];
         this.result = false;
+
+        //reset forms
         $scope.form.$setUntouched();
         $scope.form.$setPristine();
     }
 
     this.submit = function() {
-        console.log(this.answer)
+
         if (!$scope.form.$error.required) {
             this.correct = 0;
 
+            //Calculate result
             this.questions.forEach((ques, index) => {
                 if (ques.answer === this.answer[index])
                     this.correct++;
             });
 
-
+            //Map Configuration
             $scope.labels = ["Right", "Wrong"];
             $scope.data = [this.correct, (this.questions.length - this.correct)];
+
+            //Show Result Pie Chart
             this.result = true;
-            console.log(this.correct);
+
         }
 
     }
@@ -45,7 +52,7 @@ export default angular.module('liftoffApp.main', [uiRouter])
             require: "ngModel",
             link: function(scope, element, attributes, ngModel) {
 
-
+                //Custom Validators to check answers
                 ngModel.$validators.evaluate = function(modelValue, viewValue) {
                     var value = modelValue || viewValue;
 
